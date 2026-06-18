feee2bf72c3cd29cb01c1a6819867df95a77eb1e
lixiaoqiang-taiyi <lixiaoqiang@airace.com.cn>
2026-06-18 14:58:39 +0800

    test(saas-api): 更新 S3CompatibleObjectStorageClientTest

--- services/saas-api/src/test/java/com/yunyan/saasapi/application/storage/S3CompatibleObjectStorageClientTest.java
+++ services/saas-api/src/test/java/com/yunyan/saasapi/application/storage/S3CompatibleObjectStorageClientTest.java
@@ -16,6 +16,7 @@ class S3CompatibleObjectStorageClientTest {
 
   @Mock private SaasAppProperties saasAppProperties;
   @Mock private LocalObjectStorageClient localObjectStorageClient;
+  @Mock private AwsS3ObjectStorageClient awsS3ObjectStorageClient;
 
   @InjectMocks private S3CompatibleObjectStorageClient client;
 
@@ -43,5 +44,20 @@ class S3CompatibleObjectStorageClientTest {
     var url = client.upload("tenant-1/pkg.zip", new byte[] {1}, "application/zip");
 
     assertThat(url).isEqualTo("file:///tmp/pkg.zip");
+  }
+
+  @Test
+  void upload_withUseRealS3_delegatesToAwsClient() {
+    var storage = new SaasAppProperties.ObjectStorage();
+    storage.setProvider("s3-compatible");
+    storage.setUseRealS3(true);
+    storage.setEndpoint("https://minio.local:9000");
+    when(saasAppProperties.getObjectStorage()).thenReturn(storage);
+    when(awsS3ObjectStorageClient.upload(eq("tenant-1/pkg.zip"), eq(new byte[] {1}), eq("application/zip")))
+        .thenReturn("s3://tenant-exports/tenant-1/pkg.zip");
+
+    var url = client.upload("tenant-1/pkg.zip", new byte[] {1}, "application/zip");
+
+    assertThat(url).isEqualTo("s3://tenant-exports/tenant-1/pkg.zip");
   }
 }

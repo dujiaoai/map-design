f21567e4fe4f1839f55eab865f49a0dcc4503706
lixiaoqiang-taiyi <lixiaoqiang@airace.com.cn>
2026-06-18 18:52:17 +0800

    feat(saas-api): SCIM Group and SyncCursor mappers

---
+++ services/saas-api/src/main/java/com/yunyan/saasapi/domain/mapper/ScimGroupMapper.java
@@ -0,0 +1,8 @@
+package com.yunyan.saasapi.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yunyan.saasapi.domain.entity.ScimGroup;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScimGroupMapper extends BaseMapper<ScimGroup> {}
